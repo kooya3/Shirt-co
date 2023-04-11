8f812574c1b2c3271bbe9d9a0d84fc5bbd12ad8a
kooya3 <elyeestatua@gmail.com>
2023-04-11 18:45:04 +0300

    updating the canvas folder

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { CustomButton } from '../components';
 import {
   headContainerAnimation,
   headContentAnimation,
