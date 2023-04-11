12b70a8d8e71a97ed19681cbf91baf3907271e02
kooya3 <elyeestatua@gmail.com>
2023-04-11 18:19:05 +0300

    imports from framer-motion & valtio

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+const Home = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Home
