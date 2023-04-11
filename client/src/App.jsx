c54ffe6fbe08f171016bcda067d45b98d0a96337
kooya3 <elyeestatua@gmail.com>
2023-04-11 18:04:54 +0300

    creating the render capability in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Home from './pages/Home';
 function App() {
 
   return (
-    <div>
-      <h1 className="head-text">Testing</h1>
-    </div>
+    <main className="app transition-all ease-in">
+     <Home />
+     <Canvas />
+     <Customizer />
+    </main>
   )
 }
 
