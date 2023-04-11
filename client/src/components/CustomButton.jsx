b419eca77a85ccb723498f854b943036eb19416a
kooya3 <elyeestatua@gmail.com>
2023-04-11 20:40:39 +0300

    created custombutton jsx file

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -24,7 +24,7 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
 
   return (
     <button
-      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      className={`px-1.8 py-1.5 flex-1 rounded-lg ${customStyles}`}
       style={generateStyle(type)}
       onClick={handleClick}
     >
