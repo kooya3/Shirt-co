9153e1f9ace79667e8b75e2ec774834c39928b9d
kooya3 <elyeestatua@gmail.com>
2023-04-11 18:41:17 +0300

    created the custombutton and animate presence effect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <main className="app transition-all ease-in">
      <Home />
-     <Canvas />
      <Customizer />
     </main>
   )
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
 import { proxy } from 'valtio';
 
 const state = proxy({
-    intro: true,
-    color: "#EFBD48",
-    isLogoTexture: true,
-    isFullTexture: false,
-    logoDecal: './threejs.png',
-    fullDecal: './threejs.png',
-})
+  intro: true,
+  color: '#EFBD48',
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: './threejs.png',
+  fullDecal: './threejs.png',
+});
 
 export default state;
