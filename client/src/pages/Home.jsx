dae789bc0a979c1dcd4945e05412325b5aab0771
kooya3 <elyeestatua@gmail.com>
2023-04-11 18:27:05 +0300

    started on valtio functionality | store dir

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
+import {
+headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation
+} from "../config/motion";
 const Home = () => {
     return (
         <div>
