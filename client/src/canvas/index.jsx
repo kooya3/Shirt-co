8744d8ef400ec2bc55d079378467103ff485cd88
kooya3 <elyeestatua@gmail.com>
2023-04-12 20:06:36 +0300

    messing with index

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -16,12 +16,12 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-     {/*  <CameraRig> */}
-        {/* <Backdrop /> */}
+      <CameraRig>
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
   )
 }
