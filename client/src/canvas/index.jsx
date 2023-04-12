0f67f3a2a5b8b47e8e51d937e78a70dd8642afcc
kooya3 <elyeestatua@gmail.com>
2023-04-12 15:31:43 +0300

    creating scene for glb shirt

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,20 +8,20 @@ import CameraRig from './CameraRig';
 const CanvasModel = () => {
   return (
     <Canvas
-      shadows
+      /* shadows
       camera={{ position: [0, 0, 0], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      className="w-full max-w-full h-full transition-all ease-in"
+      className="w-full max-w-full h-full transition-all ease-in" */
     >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      <CameraRig>
-        <Backdrop />
+     {/*  <CameraRig> */}
+        {/* <Backdrop /> */}
         <Center>
           <Shirt />
         </Center>
-      </CameraRig>
+      {/* </CameraRig> */}
     </Canvas>
   )
 }
