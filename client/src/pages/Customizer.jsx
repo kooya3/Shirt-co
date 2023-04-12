c2048b1be36863c6fd21ff95479c3d49ba403c54
kooya3 <elyeestatua@gmail.com>
2023-04-12 13:03:28 +0300

    created the custom styles for the button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,6 +48,8 @@ const Customizer = () => {
           <CustomButton 
           type="filled"
           title="Go Back"
+          handleClick={() => state.intro = true}
+          customStyles="w-fit px-4 py-2.5 font-bold text-sm"
           />
 
         </motion.div>
