6a2dc125cddaa776cbfab753157fcad44f4b456a
kooya3 <elyeestatua@gmail.com>
2023-04-12 14:51:22 +0300

    created the canvas module and mesh for the shirt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-/* import Canvas from './canvas'; */
+import Canvas from './canvas';
 import Customizer from './pages/Customizer';
 import Home from './pages/Home';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
-      {/* <Canvas /> */}
+      <Canvas />
       <Customizer />
     </main>
   )
