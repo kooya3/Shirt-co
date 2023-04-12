607c938c0569889e57452b61598581b10e5104cf
kooya3 <elyeestatua@gmail.com>
2023-04-12 14:42:57 +0300

    creating textures and 3d shirt

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import state from '../store';
+import { Decal, useTexture, useGLTF } from '@react-three/drei';
 const Shirt = () => {
   return (
     <div>Shirt</div>
