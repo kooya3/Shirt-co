45c1bde77b0ecb1b061bbc030a2a809b44527b43
kooya3 <elyeestatua@gmail.com>
2023-04-12 13:13:08 +0300

    editing main root files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import { Canvas } from './canvas';
+import Canvas from './canvas';
 import Customizer from './pages/Customizer';
 import Home from './pages/Home';
 
 function App() {
-
   return (
     <main className="app transition-all ease-in">
-     <Home />
-     <Canvas />
-     <Customizer />
+      <Home />
+      <Canvas />
+      <Customizer />
     </main>
   )
 }
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -42,15 +42,15 @@ const Customizer = () => {
 
 
         <motion.div
-        className="absolute z-10 top-5 right-5"
-        {...fadeAnimation}
-        >
-          <CustomButton 
-          type="filled"
-          title="Go Back"
-          handleClick={() => state.intro = true}
-          customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-          />
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton 
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
 
         </motion.div>
 
