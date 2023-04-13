3e37a82bef848f42440b9362f6087c2f9cfb7e16
kooya3 <elyeestatua@gmail.com>
2023-04-13 13:00:09 +0300

    created the last custom button

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -27,7 +27,13 @@ const FilePicker = ({ file, setFile, readFile }) => {
           handleClick={() => readFile('logo')}
           customStyles="text-xs"
         />
-        
+
+        <CustomButton 
+          type="filled"
+          title="Full"
+          handleClick={() => readFile('full')}
+          customStyles="text-xs"
+        />
       </div>
     </div>
   )
