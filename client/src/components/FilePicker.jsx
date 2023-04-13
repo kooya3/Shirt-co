f040156ae3dfa2e1dab407a62b178a174f5bfeb8
kooya3 <elyeestatua@gmail.com>
2023-04-13 12:59:09 +0300

    created a custombutton

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -19,6 +19,15 @@ const FilePicker = ({ file, setFile, readFile }) => {
           {file === '' ? "No file selected" : file.name}
         </p>
       </div>
+
+      <div className="mt-4 flex flex-wrap gap-3">
+        <CustomButton 
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile('logo')}
+          customStyles="text-xs"
+        />
+        
       </div>
     </div>
   )
