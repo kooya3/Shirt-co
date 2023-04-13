8f06634d583aa2f0ac214dfe01086ef664049bad
kooya3 <elyeestatua@gmail.com>
2023-04-13 12:44:36 +0300

    created decalType for layering texture

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,6 +54,17 @@ const generateTabContent = () => {
   }
 }
 
+const handleDecals = (type, result) => {
+  const decalType = DecalTypes[type];
+
+  state[decalType.stateProperty] = result;
+
+  if(!activeFilterTab[decalType.filterTab]) {
+    handleActiveFilterTab(decalType.filterTab)
+  }
+}
+
+
 const readFile = (type) => {
   reader(file)
     .then((result) => {
