c73af36ab416de3e3c4d4f007e1a56992a8173f5
kooya3 <elyeestatua@gmail.com>
2023-04-13 12:56:45 +0300

    creating the file picker functionality

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const FilePicker = () => {
+const FilePicker = ({ file, setFile, readFile }) => {
   return (
-    <div>FilePicker</div>
+    <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
+        <input 
+        id="file-upload"
+        type="file"
+        accept="image/*"
+        onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+      </div>
+    </div>
   )
 }
 
