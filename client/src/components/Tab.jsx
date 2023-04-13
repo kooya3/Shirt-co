9c4e881f84f2e58a191c4e9ddeafd7cb4940ea9b
kooya3 <elyeestatua@gmail.com>
2023-04-13 10:27:56 +0300

    creating the sttyling for each tab

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -5,7 +5,10 @@ import state from '../store';
 
 const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
-  
+
+  const activeStyles = isFilterTab && isActiveTab
+  ? { backgroundColor: snap.color, opacity: 0.5 }
+  ? { backgroundColor: "transparent", opacity: 1 } 
   return (
     <div>Tab</div>
   )
