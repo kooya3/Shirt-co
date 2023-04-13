f8375c7dd8fe585cac640924c3b21d75ee75ca88
kooya3 <elyeestatua@gmail.com>
2023-04-13 11:50:26 +0300

    creating the colorPicker tab

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { SketchPicker } from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 const ColorPicker = () => {
+const snap = useSnapshot(state);
   return (
     <div>ColorPicker</div>
   )
