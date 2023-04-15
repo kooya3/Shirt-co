29dda9bb01918619f58a1104453c5aa116fd990a
kooya3 <elyeestatua@gmail.com>
2023-04-15 09:39:46 +0300

    setting request limit and get routes

--- server/index.js
+++ server/index.js
@@ -7,4 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+
+app.use(express.json({ limit: "50mb" }))
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E"})
 })
