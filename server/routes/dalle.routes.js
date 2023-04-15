ee0a5cda266cde5f56aa9a85d683e020d9476a21
kooya3 <elyeestatua@gmail.com>
2023-04-15 09:47:16 +0300

    creating env variable routing

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,3 +1,12 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi} from 'openai';
+
+
+dotenv.config();
+
+const router = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
