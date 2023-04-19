b018baa36ae1e7bd431f31268537a2578d9181dc
kooya3 <elyeestatua@gmail.com>
2023-04-19 13:18:48 +0300

    created last edits

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination🖤❤️</strong>{" "} and define your own style.✨
               </p>
 
               <CustomButton 
