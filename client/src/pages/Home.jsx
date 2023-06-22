a8975f1870c4ec83ffb6962354f0278eaaff9a9c
kooya3 <elyeestatua@gmail.com>
2023-06-22 17:49:40 +0300

    update on logo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                LET'S <br className="xl:block hidden" /> DO IT.♥️
               </h1>
             </motion.div>
             <motion.div
@@ -36,7 +36,7 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination🖤❤️</strong>{" "} and define your own style.✨
+              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination🖤</strong>{" "} and define your own style.✨
               </p>
 
               <CustomButton 
